63a7ed55199f7957ca47f7224b6e8a56c935b87b
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-07-04 10:06:35 +0530

    practice SQL prob

--- SQL/Basic/Weather Observation Station 7.sql
+++ SQL/Basic/Weather Observation Station 7.sql
@@ -1,4 +1,3 @@
-
 /*
     Enter your query here and follow these instructions:
     1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
@@ -7,3 +6,16 @@
 */
 SELECT DISTINCT(CITY) FROM STATION
 WHERE CITY LIKE '%a' or CITY LIKE '%e' or CITY LIKE '%i' or CITY LIKE '%o' or CITY LIKE '%u';
+
+/*
+Better version
+*/
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+SELECT DISTINCT(CITY) FROM STATION
+WHERE CITY REGEXP '[aeiouAEIOU]$';
