e5f71cde14513d3de70f6e59c556b50eebe6c1df
Tushar Sharma <165460118+tushxr12@users.noreply.github.com>
2025-07-04 10:02:18 +0530

    practice sql prob

--- SQL/Basic/Weather Observation Station 6.sql
+++ SQL/Basic/Weather Observation Station 6.sql
@@ -3,3 +3,12 @@ Enter your query here.
 */
 SELECT DISTINCT(CITY) FROM STATION
 WHERE CITY LIKE 'a%' or CITY LIKE 'e%' or CITY LIKE 'i%' or CITY LIKE 'o%' or CITY LIKE 'u%';
+
+/*
+Better version
+*/
+/*
+Enter your query here.
+*/
+SELECT DISTINCT(CITY) FROM STATION
+WHERE CITY REGEXP '^[aeiouAEIOU]';
